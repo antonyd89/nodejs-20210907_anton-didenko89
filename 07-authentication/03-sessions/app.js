0ad1b4511be3fa68d001622a7d8c7e1da10db3ca
Anton Didenko <anton.didenko89@gmail.com>
2021-10-23 17:38:51 +0300

    3rd task from 7 module is solved

--- 07-authentication/03-sessions/app.js
+++ 07-authentication/03-sessions/app.js
@@ -45,6 +45,18 @@ router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
   if (!header) return next();
 
+  const token = header.split(' ')[1];
+  if (!token) return next();
+
+  const session = await Session.findOne({"token": token}).populate('user');
+  if (!session) {
+    ctx.throw(401, 'Неверный аутентификационный токен');
+  }
+
+  session.lastVisit = new Date();
+  await session.save();
+
+  ctx.user = session.user;
   return next();
 });
 
@@ -53,7 +65,7 @@ router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
 
